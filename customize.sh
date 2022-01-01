5bab7377f2d027faf83472aaf140b9640d4f3f29
Xinlly <1678796546@qq.com>
2022-01-01 18:16:10 +0800

    正式使用

--- customize.sh
+++ customize.sh
@@ -3,6 +3,6 @@
 varPath=${0%/*}
 source ${varPath}/var.sh
 
-echo "WorkDir: ${WorkDir}"
+#echo "WorkDir: ${WorkDir}"
 #mkdir mountRoot
 [ ! -d ${mountRoot} ] && mkdir -p ${mountRoot} && log "[i]" "[Mkdir]" "${mountRoot}" && chown root:sdcard_rw ${mountRoot} && touch ${mountRoot}/.nomedia && chown root:sdcard_rw ${mountRoot}/.nomedia && chmod 0775 ${mountRoot}/.nomedia
