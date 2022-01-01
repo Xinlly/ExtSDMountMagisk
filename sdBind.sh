5b25c83abbe520adac984b6a2f30b2f7fd45ba6f
Xinlly <1678796546@qq.com>
2022-01-01 19:43:56 +0800

    。。。

--- sdBind.sh
+++ sdBind.sh
@@ -1,7 +1,34 @@
 #!/system/bin/sh
+logFile=/data/adb/00Sev/sdBind/log/sdBind.log
+runtimeR=/mnt/runtime/read
+runtimeW=/mnt/runtime/write
+runtimeD=/mnt/runtime/default
+runtimeSheet=(${runtimeR} ${runtimeW} ${runtimeD})
+DATA_MEDIA=/data/media
+profile=0
+#mountDevice=/dev/block/mmcblk1p1
+mountDevice=/data/adb/00Sev/emulated/0
+mountRootName=expandSev
+mountRoot=/mnt/${mountRootName}
+mountPoint=${mountRoot}/${profile}
+sdBindListTemp=(source direct)
+sdBindList=(\
+	Backup Backup\
+	Documents Documents\
+	Download Download\
+	Music Music\
+	#Download/Join Android/data/com.joaomgcd.join/sdcard/Join\
+	#00Real/Music/Netease netease/cloudmusic/Music\
+	Pictures Pictures\
+	Pictures/JDImage Android/data/com.jingdong.app.mall/sdcard/Pictures/JDImage\
+	DCIM DCIM\
+	SwiftBackup SwiftBackup\
+)
 
-varPath=${0%/*}
-source ${varPath}/var.sh
+
+log(){
+	echo "[${1}] $(date "+%Y/%m/%d %H:%M:%S") ${2}\t\t${3}" >>${logFile} 2>&1
+}
 
 mkRoot () {
 	#log "i" "[checking]" "${mountRoot}"
