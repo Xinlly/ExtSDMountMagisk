8f0482b9d24ef10a547e2b1fcb338023d635f68e
Xinlly <1678796546@qq.com>
2022-01-01 18:57:47 +0800

    修正文件错误

--- customize.sh
+++
@@ -1,8 +0,0 @@
-#!/system/bin/sh
-
-varPath=${0%/*}
-source ${varPath}/var.sh
-
-#echo "WorkDir: ${WorkDir}"
-#mkdir mountRoot
-[ ! -d ${mountRoot} ] && mkdir -p ${mountRoot} && log "[i]" "[Mkdir]" "${mountRoot}" && chown root:sdcard_rw ${mountRoot} && touch ${mountRoot}/.nomedia && chown root:sdcard_rw ${mountRoot}/.nomedia && chmod 0775 ${mountRoot}/.nomedia
